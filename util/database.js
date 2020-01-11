a6964982862f430a4753f66f03ca147b2888de67
david lavieri <dvlavieri@gmail.com>
2020-01-11 11:23:00 -0800

    add sequelize-cli config

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
 const Sequelize = require('sequelize');
+var sequelize;
 
-const sequelize = new Sequelize('jamify-muzik', 'davidlavieri', 'lavierinode', {
-    host: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    omitNull: true,
-});
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres'
+
+    })
+} else {
+    sequelize = new Sequelize('jamify-muzik', 'davidlavieri', 'lavierinode', {
+        host: process.env.DATABASE_URL,
+        dialect: 'postgres',
+        protocol: 'postgres',
+        omitNull: true,
+    });
+}
 
 module.exports = sequelize;
