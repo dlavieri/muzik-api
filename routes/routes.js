f6397ab87e0eead7570ce4e8d53e51fb19aa8f38
david lavieri <dvlavieri@gmail.com>
2020-01-23 09:22:07 -0800

    temp removing authorization:

--- routes/routes.js
+++ routes/routes.js
@@ -3,21 +3,21 @@ const apiRoutes = require('../controllers/index');
 const isAuth = require('../controllers/auth');
 
 const router = express.Router();
-
-router.get('/get-home-moods', isAuth, apiRoutes.getHome);
-
-router.get('/moods/:moodId', isAuth, apiRoutes.getMood);
+// add back in isAuth
+router.get('/get-home-moods', apiRoutes.getHome);
+// add back in isAuth
+router.get('/moods/:moodId', apiRoutes.getMood);
 
 router.post('/add-music', apiRoutes.postSong);
 
 router.post('/new-user', apiRoutes.newUser);
 
 router.post('/login', apiRoutes.login);
-
-router.post('/love-song/:songId', isAuth, apiRoutes.postAddSongToPlaylist);
-
-router.get('/playlists/:playlistname', isAuth, apiRoutes.getPlaylist);
-
-router.post('/playlists/remove-song/:songId', isAuth, apiRoutes.postRemoveSongFromPlaylist);
+// add back in isAuth
+router.post('/love-song/:songId', apiRoutes.postAddSongToPlaylist);
+// add back in isAuth
+router.get('/playlists/:playlistname', apiRoutes.getPlaylist);
+// add back in isAuth
+router.post('/playlists/remove-song/:songId',  apiRoutes.postRemoveSongFromPlaylist);
 
 module.exports = router;
