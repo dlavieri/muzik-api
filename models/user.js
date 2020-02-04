ea96504b512126ae8493e6a260bef0add365969d
david lavieri <dvlavieri@gmail.com>
2020-02-04 11:36:48 -0800

    temp change to user model

--- models/user.js
+++ models/user.js
@@ -12,18 +12,18 @@ const User = sequelize.define('user', {
         type: Sequelize.STRING,
         allowNull: false
     },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: true
-    },
-    location: {
-        type: Sequelize.STRING,
-        allowNull: true
-    },
-    img: {
-        type: Sequelize.STRING,
-        allowNull: true
-    }
+    // name: {
+    //     type: Sequelize.STRING,
+    //     allowNull: true
+    // },
+    // location: {
+    //     type: Sequelize.STRING,
+    //     allowNull: true
+    // },
+    // img: {
+    //     type: Sequelize.STRING,
+    //     allowNull: true
+    // }
 });
 
 
